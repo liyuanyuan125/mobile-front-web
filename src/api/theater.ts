1435ef53ef8219927d3725271197aa8ed9ce5cfc
wangxiaoxiao <xiaoxiao.wang@piaoshen.com>
2019-08-23 10:01:49 +0800

    Feature: error fix

--- src/api/theater.ts
+++ src/api/theater.ts
@@ -1,21 +1,21 @@
-// import { get , post , put , del } from '@/fn/ajax'
+import { get , post , put , del } from '@/fn/ajax'
 
 // 获取验证码
-// export async function getCode(query: any) {
-//   const res = await post('/account/login_get_sms_code' , query)
-//   return res
-// }
+export async function getCode(query: any) {
+  const res = await post('/account/login_get_sms_code' , query)
+  return res
+}
 
 // 校验验证码
-// export async function verifyCode(query: any) {
-//   const res = await post('/account/login_verify_sms_code' , query)
-//   return res
-// }
+export async function verifyCode(query: any) {
+  const res = await post('/account/login_verify_sms_code' , query)
+  return res
+}
 
 // 修改密码
-// export async function setPwd(query: any) {
-//   const res = await post('/account/set_password' , query)
-//   return res
-// }
+export async function setPwd(query: any) {
+  const res = await post('/account/set_password' , query)
+  return res
+}
 
 

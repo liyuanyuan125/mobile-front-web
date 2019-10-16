7dc362501cb783dbfddd64e66ffd053b9fa3d1d9
yingying.jiang <yingying.jiang@piaoshen.com>
2019-10-16 14:29:14 +0800

    Feature:完善广告片详情页

--- src/views/video/components/sampleArea.vue
+++ src/views/video/components/sampleArea.vue
@@ -2,8 +2,12 @@
   <!--广告片信息 -->
   <div class="videosample">
     <h4>广告小样</h4>
-    <div>
-      <img />
+    <div class="videobox" @click="playVideo">
+      <!-- <span class="info">{{sampleInfo.videoName}}</span> -->
+      <span class="play" v-if="sampleInfo.videoCoverUrl.url"></span>
+      <img :src="coverImg" v-if="sampleInfo.videoCoverUrl.url" />
+      <img src="../assets/cover.png" v-if="!sampleInfo.videoCoverUrl.url" />
+      <video :src="sampleInfo.videoUrl" ref="video"></video>
     </div>
   </div>
 </template>
@@ -18,8 +22,21 @@ import ViewBase from '@/util/ViewBase'
 export default class SampleArea extends ViewBase {
   @Prop({ type: Object }) sampleInfo!: any
 
-  mounted() {
-    // todo
+  coverImg: string = ''
+
+  created() {
+    // 处理封面图
+    const coverUrl = this.sampleInfo.videoCoverUrl
+    if (coverUrl.type == 'piaoshen') {
+      this.coverImg = coverUrl.url ? imgProxy(coverUrl.url, 160, 210) : ''
+    } else {
+      this.coverImg = coverUrl.url
+    }
+  }
+
+  playVideo() {
+    const vid: any = this.$refs.video
+    vid.play()
   }
 }
 </script>
--- src/views/video/index.vue
+++ src/views/video/index.vue
@@ -5,8 +5,8 @@
     <SampleArea :sampleInfo="videoDetail.adSampleInfo" v-if="videoDetail.adSampleInfo.videoUrl" />
     <!-- <PayInfoArea /> -->
     <BaseInfoArea :baseInfo="videoDetail.adBaseInfo" />
-    <button class="btncancel" v-if="videoCancle">取消</button>
-    <button class="btncancel" v-if="videoCanDel && !videoCancle">删除广告片</button>
+    <!-- <button class="btncancel" @click="videoAlert('cancel')" v-if="videoCancel">取消</button> -->
+    <button class="btncancel" @click="videoAlert('delete')" v-if="videoCanDel">删除广告片</button>
   </div>
 </template>
 
@@ -17,8 +17,9 @@ import VideoInfoArea from './components/videoInfoArea.vue'
 import SampleArea from './components/sampleArea.vue'
 import PayInfoArea from './components/payArea.vue'
 import BaseInfoArea from './components/baseInfoArea.vue'
-import { getVideoDetail } from '@/api/advertiser.ts'
+import { getVideoDetail, cancelVideoDetail, delVideoDetail } from '@/api/advertiser.ts'
 import { toast } from '@/util/toast'
+import { Dialog } from 'vant'
 
 @Component({
   components: {
@@ -30,38 +31,23 @@ import { toast } from '@/util/toast'
   }
 })
 export default class ResultReport extends Vue {
-  adId: string = ''
+  videoId: string = ''
   videoDetail: any = null
-  videoCancle: boolean = true
-  videoCanDel: boolean = false
+  videoCanDel: boolean = true
 
   beforeMount() {
-    const vid = this.$route.params.adId
-    this.adId = vid
+    const vid = this.$route.params.videoId
+    this.videoId = vid
     this.getVideoDetail(vid)
   }
 
-  // 获取报告详情  468
+  // 获取报告详情  468 待转码和已完成不能删除
   async getVideoDetail(adVideoId: string) {
     try {
       const res: any = await getVideoDetail({ adVideoId })
       if (res.code === 0) {
         this.videoDetail = res.data
-        switch (res.data.adVideoStatusInfo.adVideoStatus) {
-          case 3:
-            // result ="待转码"
-            this.videoCancle = false
-            break
-          case 4:
-            // result = '已完成'
-            this.videoCancle = false
-            break
-          case 6:
-            // result = '已取消'
-            this.videoCancle = false
-            this.videoCanDel = true
-            break
-        }
+        this.videoCanDel = res.data.adVideoStatusInfo.hasCanDelete
         // case 0:result = "未知"
         // case 1:result = "待审批"
         // case 2: result ="待支付"
@@ -69,6 +55,79 @@ export default class ResultReport extends Vue {
         // case 4: result ="已完成"
         // case 5: result ="未通过"
         // case 6: result ="已取消"
+      } else {
+        toast(res.msg)
+      }
+    } catch (ex) {
+      toast(ex)
+    }
+  }
+
+  // 确认弹框
+  videoAlert(type: string) {
+    switch (type) {
+      case 'delete':
+        Dialog.confirm({
+          message: '删除后不可恢复，是否继续？',
+          showConfirmButton: true,
+          confirmButtonText: '确认删除',
+          showCancelButton: true,
+          cancelButtonText: '暂不删除',
+          className: 'videotip'
+        })
+          .then(() => {
+            this.delVideo()
+          })
+          .catch(() => {
+            console.log('点击了取消按钮噢')
+          })
+        break
+      case 'cancel':
+        Dialog.confirm({
+          message: '取消后不可恢复，是否继续？',
+          showConfirmButton: true,
+          confirmButtonText: '确认取消',
+          showCancelButton: true,
+          cancelButtonText: '暂不取消',
+          className: 'videotip'
+        })
+          .then(() => {
+            this.cancleVideo()
+          })
+          .catch(() => {
+            // console.log('点击了取消按钮噢')
+          })
+        break
+    }
+  }
+
+  // 取消广告片
+  async cancleVideo() {
+    try {
+      const res: any = await cancelVideoDetail({ adVideoId: this.videoId })
+      if (res.code === 0) {
+        toast('取消成功')
+        this.videoCancel = false
+        this.videoCanDel = true
+        console(res)
+      } else {
+        toast(res.msg)
+      }
+    } catch (ex) {
+      toast(ex)
+    }
+  }
+
+  // 删除广告片
+  async delVideo() {
+    try {
+      const res: any = await delVideoDetail({ adVideoId: this.videoId })
+      if (res.code === 0) {
+        toast('删除成功')
+        setTimeout(() => {
+          console.log('回列表页')
+        }, 1500)
+        // console(res)
       } else {
         toast(res.msg)
       }

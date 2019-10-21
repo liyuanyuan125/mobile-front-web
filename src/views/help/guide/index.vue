82649598867dc8dd9d2cf655e1c30c0f86035a85
yingying.jiang <yingying.jiang@piaoshen.com>
2019-10-21 22:27:25 +0800

    Fixed:同上

--- src/views/help/guide/index.vue
+++ src/views/help/guide/index.vue
@@ -39,10 +39,10 @@
     <div class="btnbox">
       <dl>
         <dd>
-          <button>回首页</button>
+          <button @click="goAppLink('home')">回首页</button>
         </dd>
         <dd>
-          <button class="kf">在线客服</button>
+          <button class="kf" @click="goAppLink('kefu')">在线客服</button>
         </dd>
       </dl>
     </div>
@@ -51,6 +51,7 @@
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
+import { openAppLinkClient } from '@/util/native'
 
 @Component({})
 export default class Guide extends Vue {
@@ -65,12 +66,8 @@ export default class Guide extends Vue {
     // 计算如何投放和查看报告的位置
     const step: any = this.$refs.step
     const report: any = this.$refs.report
-    // const clientHeight: number = document.documentElement.clientHeight
-    // const appHeight: any = document.getElementsByClassName('app')[0].scrollHeight
-    // const diff = appHeight - clientHeight
     this.scrollStep = step.offsetTop
     this.scrollReport = report.offsetTop
-    console.log('top', this.scrollReport)
   }
 
   destroyed() {
@@ -79,9 +76,8 @@ export default class Guide extends Vue {
 
   // 切换tab
   changeTab(val: number) {
-    console.log(val)
     this.tabIdx = val
-    switch (val) {
+    switch (this.tabIdx) {
       case 1:
         document.documentElement.scrollTop = 0
         break
@@ -109,6 +105,20 @@ export default class Guide extends Vue {
     if (topNum > this.scrollReport) {
       this.tabIdx = 3
     }
+  }
+
+  // applink
+  async goAppLink(type: string) {
+    let applink = 'homePage'
+    if (type === 'kefu') {
+      applink = 'hotLinePage'
+    }
+    const objectData = {
+      applinkData: 'jydataadvertiser://scheme?p=' + applink,
+      originUrl: location.href
+    }
+    const obj = { params: objectData }
+    await openAppLinkClient(obj)
   }
 }
 </script>

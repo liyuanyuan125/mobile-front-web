6b4e9fda23c8d2c3896d72388abbbc74a3a6194a
张鹏 <lip2up@qq.com>
2020-03-11 16:06:45 +0800

    优化 chinaMap 组件

--- src/views/demo/chinaMap.vue
+++ src/views/demo/chinaMap.vue
@@ -2,8 +2,6 @@
   <div class="demo-page">
     <ChinaMap
       :data="data"
-      :minValue="100"
-      :maxValue="10000"
       :borderColor="borderColorList[theme]"
       :colorRange="colorRangeList[theme]"
       class="china-map"
@@ -31,36 +29,36 @@ import { Button } from 'vant'
 })
 export default class extends Vue {
   data: ChinaMapItem[] = [
-    { name: '新疆', value: 1000 },
-    { name: '西藏', value: 2000 },
-    { name: '青海', value: 3000 },
-    { name: '甘肃', value: 4000 },
-    { name: '内蒙古', value: 5000 },
-    { name: '宁夏', value: 8000 },
-    { name: '陕西', value: 6000 },
-    { name: '黑龙江', value: 8000 },
-    { name: '吉林', value: 3000 },
-    { name: '辽宁', value: 5000 },
-    { name: '河南', value: 9999 },
-    { name: '北京', value: 8888 },
-    { name: '天津', value: 6666 },
-    { name: '江苏', value: 6666 },
-    { name: '安徽', value: 8888 },
-    { name: '湖北', value: 3888 },
-    { name: '湖南', value: 5888 },
-    { name: '河北', value: 3888 },
-    { name: '山西', value: 4888 },
-    { name: '四川', value: 2888 },
-    { name: '广东', value: 5200 },
-    { name: '山东', value: 8200 },
-    { name: '广西', value: 8200 },
-    { name: '重庆', value: 8200 },
-    { name: '江西', value: 5200 },
-    { name: '浙江', value: 5200 },
-    { name: '福建', value: 8200 },
-    { name: '台湾', value: 9200 },
-    { name: '云南', value: 9200 },
-    { name: '贵州', value: 6200 },
+    { name: '新疆', value: 0 },
+    { name: '西藏', value: 20.8 },
+    { name: '青海', value: 30 },
+    { name: '甘肃', value: 40 },
+    { name: '内蒙古', value: 50 },
+    { name: '宁夏', value: 80.88 },
+    { name: '陕西', value: 60.99 },
+    { name: '黑龙江', value: 80 },
+    { name: '吉林', value: 30 },
+    { name: '辽宁', value: 50 },
+    { name: '河南', value: 99 },
+    { name: '北京', value: 88.66 },
+    { name: '天津', value: 66.66 },
+    { name: '江苏', value: 66.66 },
+    { name: '安徽', value: 88.88 },
+    { name: '湖北', value: 38.88 },
+    { name: '湖南', value: 58.88 },
+    { name: '河北', value: 38.88 },
+    { name: '山西', value: 48.88 },
+    { name: '四川', value: 28.88 },
+    { name: '广东', value: 52.88 },
+    { name: '山东', value: 82.22 },
+    { name: '广西', value: 82.33 },
+    { name: '重庆', value: 82.55 },
+    { name: '江西', value: 52.88 },
+    { name: '浙江', value: 52.22 },
+    { name: '福建', value: 82 },
+    { name: '台湾', value: 92 },
+    { name: '云南', value: 92 },
+    { name: '贵州', value: 62 },
   ]
 
   theme = 0

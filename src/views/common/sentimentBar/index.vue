3750eb4906f1d20e00ebf323c8f08451157e4c68
yingying.jiang <yingying.jiang@mtime.com>
2020-03-13 22:24:51 +0800

    Feat: update
    1.修改了一下 bubble的字号，兼容了1,123.3万的情况
    2.顶部 topbar 新增 pk 和 digg
    3.电影页新增基本信息样式

--- src/views/common/sentimentBar/index.vue
+++ src/views/common/sentimentBar/index.vue
@@ -1,7 +1,18 @@
 <template>
-  <div :class="topbarClass" :style="attribute.cusStyle">
+  <div class="topbar" :style="attribute.cusStyle">
     <span class="reBack" @click="goBack"></span>
-    <h1 :class="['title']" :v-show="isFixed">{{attribute.title}}</h1>
+    <h1 :class="['title']" v-show="titleShow">{{attribute.title}}</h1>
+    <div class="tool">
+      <i class="ico-pk"></i>
+      <i class="ico-digg"></i>
+    </div>
+    <svg width="30px" height="30px" version="1.1" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M30,89 C13.4314575,89 0,102.431458 0,119 L0,89 L30,89 Z"
+        transform="translate(0, -89)"
+        fill="#F7F7F9"
+      />
+    </svg>
   </div>
 </template>
 
@@ -16,15 +27,12 @@ export default class SentimentBar extends Vue {
    * 属性示例
    */
   @Prop({ type: Object, default: {} }) attribute!: SentimentBarItem // 基本属性
-  @Prop({ type: Boolean, default: false }) isFixed!: boolean // 是否吸顶
+  @Prop({ type: Boolean, default: false }) titleShow!: boolean // 是否显示标题
 
   topbarClass: string = 'topbar'
 
-  created() {
-    // 判断 topbar class
-    if (this.isFixed) {
-      this.topbarClass = 'topbar fixed'
-    }
+  mounted() {
+    // todo
   }
 
   // 返回上一页
@@ -53,16 +61,37 @@ export default class SentimentBar extends Vue {
 .topbar {
   height: 88px;
   line-height: 88px;
-  position: relative;
   background-color: #f7f7f9;
   border-radius: 0 0 44px 0;
-}
-.fixed {
   position: fixed;
   width: 100%;
   top: 0;
   left: 0;
-  border-radius: 0;
+  svg {
+    position: absolute;
+    left: 0;
+    top: 88px;
+  }
+}
+.tool {
+  height: 88px;
+  position: absolute;
+  right: 5px;
+  top: 0;
+  i {
+    width: 88px;
+    height: 88px;
+    display: inline-block;
+    vertical-align: baseline;
+    background: no-repeat center center;
+    background-size: 100%;
+  }
+  .ico-digg {
+    background-image: url('../../../assets/sentiment/bar-digg.png');
+  }
+  .ico-pk {
+    background-image: url('../../../assets/sentiment/bar-pk.png');
+  }
 }
 .title {
   color: #303030;
--- src/views/sentiment/movie/index.vue
+++ src/views/sentiment/movie/index.vue
@@ -1,22 +1,51 @@
 <template>
-  <SentimentBar :attribute="topbar" />
+  <div>
+    <SentimentBar :attribute="topbar" />
+    <BaseInfoArea :baseInfo="movieInfo" :overView="movieOverView" />
+  </div>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Watch } from 'vue-property-decorator'
 import ViewBase from '@/util/ViewBase'
 import SentimentBar from '@/views/common/sentimentBar/index.vue'
+import BaseInfoArea from './components/movieInfo.vue'
+// import hotLine from '@/components/hotLine'
 
 @Component({
   components: {
-    SentimentBar
+    SentimentBar,
+    BaseInfoArea
   }
 })
 export default class MoviePage extends ViewBase {
+  // topbar
   topbar = {
     title: '流浪地球',
     diggShow: true,
     pkShow: true
+  }
+  movieInfo = {
+    movieNameCn: '流浪地球',
+    movieNameEn: 'The Wandering Earth',
+    coverUrl: {
+      url:
+        'http://piaoshen.oss-cn-beijing.aliyuncs.com/images/movie/2019/05/06/190506000002357372.jpg',
+      source: 'piaoshen'
+    },
+    duration: '115分钟',
+    genreName: '剧情 / 喜剧',
+    releaseDate: '2020-02-22 中国大陆上映',
+    movieId: '100038',
+    favorable: 'B+'
+  }
+  movieOverView = {
+    heatRanking: 'NO.125',
+    commnetTrend: 121344,
+    commentCount: '1,324',
+    materialsTrend: -132332,
+    materialsCount: '1,213.3万',
+    heatTrend: 0
   }
 }
 </script>

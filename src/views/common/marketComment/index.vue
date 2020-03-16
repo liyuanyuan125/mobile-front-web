fa7d2ff135f2511c6e25ac39ae61874b28465614
陈鹏宇 <johndoe@example.com>
2020-03-16 17:21:19 +0800

    feat: 组件参数 定义

--- src/views/common/marketComment/index.vue
+++ src/views/common/marketComment/index.vue
@@ -2,11 +2,12 @@
   <div class="options-page">
     <div class="options-h3">
         <div>{{title}}</div>
+        <router-link class="more" :to="href" ><Icon name="arrow" size="18" /></router-link>
     </div>
     <div class="options-top">
         <div>
             <span class="hot">{{hotQuery.text}}
-                <img width="16px" height="16px" style="vertical-align: middle" src="../images/question.png" alt="" srcset="">
+                <img width="16px" height="16px" style="vertical-align: middle" src="@/assets/options/question.png" alt="" srcset="">
             </span>
             <div class="hot-degrees">{{hotQuery.value}}</div>
         </div>
@@ -28,7 +29,7 @@
     <div class="options-bottom">
         <div class="hot-box">
             <div class="hot-box-left">
-                <img width="13px" height="16px" src="../images/hot.png" alt="" srcset="">
+                <img width="13px" height="16px" src="@/assets/options//hot.png" alt="" srcset="">
                 全网热词
             </div>
             <div class="hot-box-right">
@@ -37,7 +38,7 @@
         </div>
         <div class="hot-box">
             <div class="hot-box-left">
-                <img width="13px" height="16px" src="../images/bad.png" alt="" srcset="">
+                <img width="13px" height="16px" src="@/assets/options//bad.png" alt="" srcset="">
                 负面热词
             </div>
             <div class="hot-box-right">
@@ -51,6 +52,7 @@
 <script lang="ts">
 import { Component, Vue, Prop } from 'vue-property-decorator'
 import { Progress, Icon } from 'vant'
+import { PublicPraise } from './type'
 
 const publicPraise = {
     appraiseList: [],
@@ -64,8 +66,12 @@ const publicPraise = {
     }
 })
 export default class Options extends Vue {
-    @Prop({ required: true, default:  () => publicPraise}) publicPraise?: any
+    /* 口碑评论数据 */
+    @Prop({ required: true, default:  () => publicPraise}) publicPraise?: PublicPraise[]
+    /* 好感度 */
     @Prop({ required: true, default:  () => ({ text: '', value: ''})}) hotQuery?: any
+    /* 跳转页面 */
+    @Prop({ required: true, default:  ({})}) href?: any
     /* 标题name */
     @Prop({ type: String, default: '口碑评论'}) title!: string
 }
--- src/views/demo/options.vue
+++ src/views/demo/options.vue
@@ -1,27 +1,30 @@
 <template>
   <div class="demo-page">
-    <contrast :fetch="fetch" :query="query" />
-    <comment :publicPraise='publicPraise' :hotQuery="hotQuery" />
-    <age :to="to" :ageRangeList='ageRangeList' />
+    <MarketContrast :fetch="fetch" :query="query" />
+    <MarketComment  :href="href" :publicPraise='publicPraise' :hotQuery="hotQuery" />
+    <AgeDistribution :href="href" :ageRangeList='ageRangeList' />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
-import contrast from '@/components/opinions/contrast/index.vue'
-import comment from '@/components/opinions/comment/index.vue'
-import Age from '@/components/opinions/ageDistribution/index.vue'
+// 口碑评论对比
+import MarketContrast from '@/views/common/marketContrast/index.vue'
+// 口碑评论
+import MarketComment from '@/views/common/marketComment/index.vue'
+// 年龄分布
+import AgeDistribution from '@/views/common/ageDistribution/index.vue'
 
 @Component({
   components: {
-    contrast,
-    comment,
-    Age
+    MarketContrast,
+    MarketComment,
+    AgeDistribution
   }
 })
 export default class DemoPage extends Vue {
     // 查看更多
-    to = {
+    href = {
         name: 'demo-twobar'
     }
     // 口碑评论 好感度

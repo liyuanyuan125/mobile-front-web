bf4a1d12dbe20ed109af84428148af4ab14cb7a8
yingying.jiang <yingying.jiang@mtime.com>
2020-03-23 16:07:50 +0800

    Feat: 完善电影竞品分析

--- src/views/common/ageDistribution/index.vue
+++ src/views/common/ageDistribution/index.vue
@@ -1,35 +1,31 @@
 <template>
   <div class="options-page">
-    <!-- <div class="contrast-h3">
-        <div>{{title}}</div>
-        <router-link class="more" :to="href" >查看更多<Icon name="arrow" size="18" /></router-link>
-    </div> -->
+    <moduleHeader title="年龄分布" tag="h4" class="module-header" />
     <div class="contrast-top">
-        <span @click="changeAge(ins)" v-for="(it, ins) in ageRangeList" :key="ins" :class="[ indexs == ins ? 'contrast-title active' : 'contrast-title']">
-            {{it.ageType}}
-        </span>
+      <span
+        @click="changeAge(ins)"
+        v-for="(it, ins) in ageList"
+        :key="ins"
+        :class="[ indexs == ins ? 'contrast-title active' : 'contrast-title']"
+      >{{it.ageType}}</span>
     </div>
     <div class="contrast-bottom">
-         <div v-for="(it, index) in ageRangeList" v-if="indexs == index" :key="index">
-            <div v-if="it.brandList && it.brandList.length > 0">
-            <div class="contrast-progress" v-for="(item, ins) in it.brandList" :key="ins">
-                <span>{{item.brandName}}</span>
-                <div class="progress">
-                    <Progress
-                        :percentage="item.brandPercent"
-                        color="#88aaf6"
-                        stroke-width="10" 
-                    />
-                    <div class="contrast-message">
-                        <div class="contrast-message-progress">{{item.brandPercent}}%</div>
-                    </div>
+      <div v-for="(it, index) in ageList" :key="index">
+        <div v-if="indexs == index">
+          <div v-if="it.rivalList && it.rivalList.length > 0">
+            <div class="contrast-progress" v-for="(item, ins) in it.rivalList" :key="ins">
+              <span>{{item.rivalName}}</span>
+              <div class="progress">
+                <Progress :percentage="item.rivalPercent" color="#88aaf6" stroke-width="10" />
+                <div class="contrast-message">
+                  <div class="contrast-message-progress" style="fontFamily: 'DIN Alternate'">{{item.rivalPercent}}%</div>
                 </div>
+              </div>
             </div>
-            </div>
-            <div v-else class="noList">
-                暂无数据
-            </div>
+          </div>
+          <div v-else class="noList">暂无数据</div>
         </div>
+      </div>
     </div>
   </div>
 </template>
@@ -38,29 +34,45 @@
 import { Component, Vue, Prop, Watch } from 'vue-property-decorator'
 import { Progress, Icon } from 'vant'
 import { AgeRangeList } from './type'
+import moduleHeader from '@/components/moduleHeader'
 
 @Component({
-    components: {
-        Progress,
-        Icon
-    }
+  components: {
+    Progress,
+    Icon,
+    moduleHeader
+  }
 })
 export default class Options extends Vue {
-    /* 年龄分布数据 */
-    @Prop({ required: true, default:  []}) ageRangeList?: AgeRangeList[]
-    /* 跳转页面 */
-    // @Prop({ required: true, default:  ({})}) href?: any
-    /* 年龄分布标题 */
-    // @Prop({ type: String, default: '年龄分布'}) title!: string
+  /* 年龄分布数据 */
+  @Prop({ required: true, default: [] }) ageRangeList?: AgeRangeList[]
+  /* 跳转页面 */
+  // @Prop({ required: true, default:  ({})}) href?: any
+  /* 年龄分布标题 */
+  // @Prop({ type: String, default: '年龄分布'}) title!: string
 
-    indexs: any = 0
-    changeAge(ins: number) {
-        this.indexs = ins
-    }
+  indexs: any = 0
+  changeAge(ins: number) {
+    this.indexs = ins
+  }
 
+  get ageList() {
+      const list: any = this.ageRangeList
+      if (list && list.length) {
+        for (const it of list) {
+            for (const el of it.rivalList) {
+                el.rivalPercent = (el.rivalPercent / 100).toFixed(1)
+            }
+        }
+      }
+      return list
+  }
 }
 </script>
 
 <style lang="less" scoped>
 @import url(./index.less);
+.module-header{
+    padding-bottom:30px;
+}
 </style>
--- src/views/common/ageDistribution/type.ts
+++ src/views/common/ageDistribution/type.ts
@@ -3,18 +3,18 @@ export interface AgeRangeList {
    * 年龄区间
    */
   appraiseList: string
-  brandList: BrandList
+  rivalList: RivalList
 }
 
-export interface BrandList {
+export interface RivalList {
   /**
    * 品牌名称
    */
-  brandName: string,
+  rivalName: string,
   /**
    * 品牌比例
    */
-  brandPercent: number
+  rivalPercent: number
 }
 
 

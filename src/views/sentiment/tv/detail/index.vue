3e464239a855880723145dec43d773d1481c89d9
yingying.jiang <yingying.jiang@mtime.com>
2020-03-26 11:22:07 +0800

    feat:备份电视剧

---
+++ src/views/sentiment/tv/detail/index.vue
@@ -0,0 +1,534 @@
+<template>
+  <div class="page">
+    <SentimentBar :title="movieInfo.movieNameCn" :sidebar="sidebar" />
+    <BaseInfoArea :baseInfo="movieInfo" :overView="movieOverView" />
+    <TabNav :list="tabList" class="formattab" />
+    <div class="hotanalysis">
+      <selectTime ref="refsTime" class="heat" />
+      <heatLineCom :overAllList="overAllHeat" :platformList="platformHeat" :params="params" />
+    </div>
+    <WantSeeTrend :dataTrend="wantSeeTrend" />
+    <BoxOffice :boxoffice="boxOffice" :link="getApplink('movieBoxOffice')" id="boxoffice" />
+    <PraiseComment
+      :favorable="movieInfo.favorable"
+      :publicPraise="publicPraise"
+      :link="getApplink('praiseHotWordsList')"
+      v-if="publicPraise.appraiseList"
+      id="praise"
+    />
+    <UserPortrait
+      :ageRangeList="userAnalysis.ageRangeList"
+      :genderList="userAnalysis.genderList"
+      id="user"
+      :link="getApplink('userAnalysis')"
+    />
+    <EventList :eventList="eventList" id="event" :link="getApplink('eventMarketingList')" />
+    <RivalAnalysis :rivalList="rivalAnalysis" id="rival" />
+    <ActorList :actorList="actorList" id="actor" :link="getApplink('actorList')" />
+    <ProduceList :produceList="produceList" :link="getApplink('produceDistribute')" />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Watch } from 'vue-property-decorator'
+import ViewBase from '@/util/ViewBase'
+import { getMovieDetail, getEventList, getRivalList } from './data'
+import SentimentBar from '@/views/common/sentimentBar/index.vue'
+import BaseInfoArea from './components/movieInfo.vue' // 影片基本信息
+import TabNav, { TabNavItem } from '@/components/tabNav'
+import { selectTime } from '@/components/hotLine'
+import heatLineCom from '@/views/common/heatLineCom/index.vue'
+import WantSeeTrend from './components/wantSeeTrend.vue' // 想看趋势
+import BoxOffice from './components/boxOffice.vue' // 影片票房
+import PraiseComment from '@/views/common/praiseComment/index.vue' // 口碑评论
+import UserPortrait from '@/views/common/user/userPortrait.vue'
+import EventList from '@/views/common/eventList/event.vue' // 事件跟踪
+import RivalAnalysis from './components/rivalAnalysis.vue' // 竞品分析
+import ActorList from '@/views/common/actorList/index.vue' // 主创人员
+import ProduceList from '@/views/common/produceList/index.vue' // 出品发行
+// import hotLine from '@/components/hotLine'
+
+@Component({
+  components: {
+    SentimentBar,
+    BaseInfoArea,
+    TabNav,
+    selectTime,
+    heatLineCom,
+    WantSeeTrend,
+    BoxOffice,
+    PraiseComment,
+    UserPortrait,
+    EventList,
+    RivalAnalysis,
+    ActorList,
+    ProduceList
+  }
+})
+export default class MoviePage extends ViewBase {
+  // 顶部 topbar 传参
+  sidebar = {
+    diggType: 3, // 1=品牌 2=艺人 3=电影 4=电视剧 5=单曲 6=专辑
+    diggId: '',
+    rivalIds: {} // 竞争对手的 url
+  }
+  // 电影id
+  movieId: string = ''
+  // 电影详细信息
+  movieInfo: any = {}
+  // 电影数据概览
+  movieOverView: any = {}
+  // 电影票房
+  boxOffice: any = {}
+  // 口碑
+  publicPraise: any = {}
+  // 用户名称
+  userAnalysis: any = {}
+  // 营销事件
+  eventList: any = {}
+  // 竞品分析
+  rivalAnalysis: any = []
+  // 主创人员
+  actorList: any = []
+  // 出品发行
+  produceList: any = []
+  // 二级导航
+  tabList: TabNavItem[] = [
+    { name: 'hot', label: '热度' },
+    { name: 'boxoffice', label: '票房' },
+    { name: 'praise', label: '口碑' },
+    { name: 'user', label: '用户' },
+    { name: 'event', label: '事件' },
+    { name: 'rival', label: '竞品' },
+    { name: 'actor', label: '资料' }
+  ]
+  // 热度分析传参
+  get params() {
+    return {
+      type: 3, // 1=品牌 2=艺人 3=电影 4=电视剧 5=单曲 6=专辑
+      id: 1, // 详情页id
+      name: '奔驰',
+      startTime: 20200304, // this.startTime,
+      endTime: 20200310 // this.endTime
+    }
+  }
+  overAllHeat = [
+    {
+      date: 1583978358078,
+      value: 123,
+      eventList: [
+        {
+          eventName: '意大利紧急求助中国',
+          eventId: '123232'
+        },
+        {
+          eventName: '意大利紧急求助中国',
+          eventId: '123232'
+        },
+        {
+          eventName: '意大利紧急求助中国',
+          eventId: '123232'
+        },
+        {
+          eventName: '意大利紧急求助中国',
+          eventId: '123232'
+        },
+        {
+          eventName: '意大利紧急求助中国',
+          eventId: '123232'
+        }
+      ]
+    },
+    {
+      date: 1583978358078,
+      value: 323,
+      eventList: [
+        {
+          eventName: '意大利紧急求助中国',
+          eventId: '123232'
+        },
+        {
+          eventName: '意大利紧急求助中国',
+          eventId: '123232'
+        },
+        {
+          eventName: '意大利紧急求助中国',
+          eventId: '123232'
+        }
+      ]
+    }
+  ]
+  platformHeat = [
+    {
+      platformName: '新浪',
+      platformValueList: [
+        {
+          name: '微博数',
+          value: '9,876'
+        },
+        {
+          name: '互动量',
+          value: '9,876.5万'
+        }
+      ],
+      platformLogo: {
+        source: 'jydata',
+        url:
+          'https://aiads-file.oss-cn-beijing.aliyuncs.com/IMAGE/ICON/aiqiyishipin.png'
+      },
+      platformId: '1',
+      platformNotice: '媒体一 媒体二 媒体三'
+    },
+    {
+      platformName: '新浪',
+      platformValueList: [
+        {
+          name: '微博数',
+          value: '9,876'
+        },
+        {
+          name: '互动量',
+          value: '9,876.5万'
+        }
+      ],
+      platformLogo: {
+        source: 'jydata',
+        url:
+          'https://aiads-file.oss-cn-beijing.aliyuncs.com/IMAGE/ICON/aiqiyishipin.png'
+      },
+      platformId: '2',
+      platformNotice: '媒体一 媒体二 媒体三'
+    },
+    {
+      platformName: '新浪',
+      platformValueList: [
+        {
+          name: '微博数',
+          value: '9,876'
+        },
+        {
+          name: '互动量',
+          value: '9,876.5万'
+        }
+      ],
+      platformLogo: {
+        source: 'jydata',
+        url:
+          'https://aiads-file.oss-cn-beijing.aliyuncs.com/IMAGE/ICON/aiqiyishipin.png'
+      },
+      platformId: '3',
+      platformNotice: '媒体一 媒体二 媒体三'
+    },
+    {
+      platformName: '新浪',
+      platformValueList: [
+        {
+          name: '微博数',
+          value: '9,876'
+        },
+        {
+          name: '互动量',
+          value: '9,876.5万'
+        }
+      ],
+      platformLogo: {
+        source: 'jydata',
+        url:
+          'https://aiads-file.oss-cn-beijing.aliyuncs.com/IMAGE/ICON/aiqiyishipin.png'
+      },
+      platformId: '4',
+      platformNotice: '媒体一 媒体二 媒体三'
+    }
+  ]
+  wantSeeTrend = {
+    dailyGainList: [
+      {
+        date: 1584622361149,
+        eventList: [
+          {
+            eventName: '花木兰首映获好评',
+            eventId: '231332'
+          },
+          {
+            eventName: '花木兰首映获好评',
+            eventId: '231332'
+          },
+          {
+            eventName: '花木兰首映获好评',
+            eventId: '231332'
+          }
+        ],
+        value: 1300
+      },
+      {
+        date: 1584622361149,
+        eventList: [
+          {
+            eventName: '花木兰首映获好评',
+            eventId: '231332'
+          },
+          {
+            eventName: '花木兰首映获好评',
+            eventId: '231332'
+          },
+          {
+            eventName: '花木兰首映获好评',
+            eventId: '231332'
+          },
+          {
+            eventName: '花木兰首映获好评',
+            eventId: '231332'
+          }
+        ],
+        value: 32132
+      },
+      {
+        date: 1584622361149,
+        eventList: [
+          {
+            eventName: '花木兰首映获好评',
+            eventId: '231332'
+          },
+          {
+            eventName: '花木兰首映获好评',
+            eventId: '231332'
+          },
+          {
+            eventName: '花木兰首映获好评',
+            eventId: '231332'
+          },
+          {
+            eventName: '花木兰首映获好评',
+            eventId: '231332'
+          },
+          {
+            eventName: '花木兰首映获好评',
+            eventId: '231332'
+          }
+        ],
+        value: 323132
+      },
+      {
+        date: 1584622361149,
+        eventList: [
+          {
+            eventName: '花木兰首映获好评',
+            eventId: '231332'
+          },
+          {
+            eventName: '花木兰首映获好评',
+            eventId: '231332'
+          },
+          {
+            eventName: '花木兰首映获好评',
+            eventId: '231332'
+          },
+          {
+            eventName: '花木兰首映获好评',
+            eventId: '231332'
+          },
+          {
+            eventName: '花木兰首映获好评',
+            eventId: '231332'
+          }
+        ],
+        value: 12313
+      },
+      {
+        date: 1584622361149,
+        eventList: [
+          {
+            eventName: '花木兰首映获好评',
+            eventId: '231332'
+          },
+          {
+            eventName: '花木兰首映获好评',
+            eventId: '231332'
+          },
+          {
+            eventName: '花木兰首映获好评',
+            eventId: '231332'
+          },
+          {
+            eventName: '花木兰首映获好评',
+            eventId: '231332'
+          },
+          {
+            eventName: '花木兰首映获好评',
+            eventId: '231332'
+          }
+        ],
+        value: 320
+      }
+    ],
+    totalGainList: [
+      {
+        date: 1583979088061,
+        eventList: [
+          {
+            eventName: '花木兰首映获好评',
+            eventId: '132323'
+          },
+          {
+            eventName: '花木兰首映获好评',
+            eventId: '132323'
+          },
+          {
+            eventName: '花木兰首映获好评',
+            eventId: '132323'
+          }
+        ],
+        value: 32311323
+      },
+      {
+        date: 1583979088061,
+        eventList: [
+          {
+            eventName: '花木兰首映获好评',
+            eventId: '132323'
+          },
+          {
+            eventName: '花木兰首映获好评',
+            eventId: '132323'
+          },
+          {
+            eventName: '花木兰首映获好评',
+            eventId: '132323'
+          }
+        ],
+        value: 32311323
+      },
+      {
+        date: 1583979088061,
+        eventList: [
+          {
+            eventName: '花木兰首映获好评',
+            eventId: '132323'
+          }
+        ],
+        value: 32311323
+      },
+      {
+        date: 1583979088061,
+        eventList: [
+          {
+            eventName: '花木兰首映获好评',
+            eventId: '132323'
+          },
+          {
+            eventName: '花木兰首映获好评',
+            eventId: '132323'
+          },
+          {
+            eventName: '花木兰首映获好评',
+            eventId: '132323'
+          },
+          {
+            eventName: '花木兰首映获好评',
+            eventId: '132323'
+          }
+        ],
+        value: 32311323
+      },
+      {
+        date: 1583979088061,
+        eventList: [
+          {
+            eventName: '花木兰首映获好评',
+            eventId: '132323'
+          }
+        ],
+        value: 32311323
+      }
+    ]
+  }
+
+  async created() {
+    this.movieId = this.$route.params.movieId
+    this.sidebar.diggId = this.movieId
+    if (this.movieId) {
+      await this.getMovieInfo()
+      await this.getEventList()
+      await this.getRivalList()
+    }
+  }
+  // api获取电影详情页
+  async getMovieInfo() {
+    const res: any = await getMovieDetail(this.movieId)
+    this.movieInfo = res.movieInfo
+    this.movieOverView = res.movieOverView
+    this.boxOffice = res.boxOffice
+    this.publicPraise = res.publicPraise
+    this.userAnalysis = res.userAnalysis
+    this.actorList = res.actorList ? res.actorList : []
+    this.produceList = res.produceList ? res.produceList : []
+    document.title = res.movieInfo.movieNameCn
+  }
+  // api获取营销事件
+  async getEventList() {
+    const res: any = await getEventList({
+      type: 3,
+      objectId: 9
+    })
+    this.eventList = res
+  }
+  // api获取竞品对手
+  async getRivalList() {
+    const res: any = await getRivalList(this.movieId)
+    this.rivalAnalysis = res
+    const ids = []
+    if (res && res.length) {
+      for (const el of res) {
+        ids.push(el.rivalId)
+      }
+    }
+    this.sidebar.rivalIds = {
+      name: 'sentimentmovierivalanalysis',
+      query: {
+        ids: ids.join(',')
+      }
+    }
+  }
+
+  /**
+   * 获取 applink
+   * 业务类型 businessType 1=品牌 2=艺人 3=电影 4=电视剧 5=单曲 6=专辑
+   * 业务Id businessObjectId
+   * @page 页面标识
+   */
+  getApplink(page: string) {
+    switch (page) {
+      case 'userAnalysis':
+        return {
+          name: 'sentimentmovieuseranalysis',
+          params: {
+            movieId: this.movieId
+          }
+        }
+      default:
+        return {
+          page,
+          businessType: 3,
+          businessObjectId: this.movieId
+        }
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.page {
+  color: #303030;
+}
+nav.formattab {
+  margin-top: 0;
+  top: 88px;
+  z-index: 11;
+}
+.hotanalysis {
+  margin-top: 40px;
+  .heat {
+    padding: 0 30px 30px;
+  }
+}
+</style>
---
+++ src/views/sentiment/tv/userAnalysis/index.vue
@@ -0,0 +1,131 @@
+<template>
+  <div class="pages">
+    <SentimentBar title="用户分析" :titleShow="true" />
+    <div class="agelist">
+      <moduleHeaer title="用户画像" />
+      <div class="item-title">性别占比</div>
+      <div class="sex">
+        <sexChart :width="annularWid" :data="sexdata" v-if="sexdata.data.length"></sexChart>
+      </div>
+      <div class="age">
+        <div class="item-title">年龄占比</div>
+        <VerticalBar :data="agedata" v-if="agedata.length" class="chart" />
+      </div>
+    </div>
+    <div class="education" v-if="educationList.data.length">
+      <moduleHeaer title="教育信息" />
+      <annularChart :data="educationList" :width="annularWid"></annularChart>
+    </div>
+    <div class="work" v-if="workList.data.length">
+      <moduleHeaer title="职业信息" />
+      <annularChart :data="workList" :width="annularWid"></annularChart>
+    </div>
+    <ProgressList :progressData="userRegionList" title="活跃地区" />
+    <ProgressList :progressData="consumePrefer" title="消费偏好" />
+    <ProgressList :progressData="moviePrefer" title="观影偏好" />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Watch } from 'vue-property-decorator'
+import ViewBase from '@/util/ViewBase'
+import { userAnalysisData } from './data'
+import ChinaMap, { ChinaMapItem } from '@/components/chinaMap'
+import { toast } from '@/util/toast'
+import sexChart from '@/components/cakeChart/sexChart.vue'
+import annularChart from '@/components/cakeChart/annularChart.vue'
+import VerticalBar, { VerticalBarItem } from '@/components/verticalBar'
+import SentimentBar from '@/views/common/sentimentBar/index.vue'
+import ProgressList from './components/progress.vue'
+import moduleHeaer from '@/components/moduleHeader'
+
+@Component({
+  components: {
+    SentimentBar,
+    sexChart,
+    annularChart,
+    VerticalBar,
+    ProgressList,
+    moduleHeaer
+  }
+})
+export default class MovieUserAnalysisPage extends ViewBase {
+  annularWid: number = document.documentElement.clientWidth - 60
+  // 性别
+  sexdata: any = {
+    data: [],
+    title: '性别占比',
+    emphasisShow: true
+  }
+  // 年龄
+  agedata: VerticalBarItem[] = []
+  // 教育
+  educationList: any = {
+    data: [],
+    color: '' // 修改颜色
+  }
+  // 职业信息
+  workList: any = {
+    data: [],
+    color: '' // 修改颜色
+  }
+  // 活跃地区
+  userRegionList: any = []
+  // 消费偏好
+  consumePrefer: any = []
+  // 观影偏好
+  moviePrefer: any = []
+
+  created() {
+    const movieId: string = this.$route.params.movieId
+    this.userAnalysis(movieId)
+  }
+
+  async userAnalysis(movieId: string) {
+    try {
+      const res: any = await userAnalysisData(movieId)
+      // 性别
+      this.sexdata.data = res.genderList
+      this.agedata = res.ageRangeList
+      this.educationList.data = res.educationList
+      this.workList.data = res.workList
+      this.userRegionList = res.userRegionList
+      this.consumePrefer = res.consumePrefer
+      this.moviePrefer = res.moviePrefer
+    } catch (ex) {
+      toast(ex)
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.pages {
+  width: 100%;
+  padding-top: 148px;
+}
+.agelist {
+  padding: 0 30px;
+  .item-title {
+    margin-top: 45px;
+    margin-bottom: 50px;
+    font-size: 34px;
+    font-weight: 500;
+    color: rgba(48, 48, 48, 1);
+    line-height: 34px;
+  }
+  .age {
+    border-top: solid 1px #d8d8d8;
+    margin: 0 30px;
+    padding-bottom: 50px;
+  }
+}
+.education {
+  border-top: 20px solid rgba(216, 216, 216, 0.2);
+  padding: 50px 30px;
+}
+.work {
+  border-top: 20px solid rgba(216, 216, 216, 0.2);
+  padding: 50px 30px;
+}
+</style>

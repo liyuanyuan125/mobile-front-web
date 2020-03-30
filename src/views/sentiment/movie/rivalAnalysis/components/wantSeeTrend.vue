fa7f26a98ec81d6a42d33968f5c488c520351d4f
yingying.jiang <yingying.jiang@mtime.com>
2020-03-30 21:42:51 +0800

    Feat:重新封装竞品对手曲线的组件

--- src/views/sentiment/movie/rivalAnalysis/components/wantSeeTrend.vue
+++ src/views/sentiment/movie/rivalAnalysis/components/wantSeeTrend.vue
@@ -18,7 +18,7 @@
       </ul>
     </div>
     <div>
-      <echartLines :lineData="lineDatas" :colors="colors" v-if="lineDatas.xDate" />
+      <trendLines :lineData="lineDatas" :colors="colors" v-if="lineDatas.xDate" />
     </div>
   </div>
 </template>
@@ -28,13 +28,13 @@ import { Component, Vue, Prop, Watch } from 'vue-property-decorator'
 import ViewBase from '@/util/ViewBase'
 import SelectDate from '@/components/selectDate'
 import { devLog, devInfo } from '@/util/dev'
-import echartLines from '@/components/hotLines'
+import trendLines from '@/components/trendLine'
 import { toast } from '@/util/toast'
 import moment from 'moment'
 
 @Component({
   components: {
-    echartLines,
+    trendLines,
     SelectDate
   }
 })

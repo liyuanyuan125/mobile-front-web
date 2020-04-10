2c30d070261bd361a2c59f55487d8ae31c126347
张鹏 <lip2up@qq.com>
2020-04-11 04:12:30 +0800

    fixbug

--- src/views/sentiment/music/components/playStats/data.ts
+++ src/views/sentiment/music/components/playStats/data.ts
@@ -4,18 +4,21 @@ import { arrayMap } from '@jydata/fe-util'
 import { MultiLineItem, MultiLineEvents, MultiLineEventHandler } from '@/components/multiLine'
 import { lastDayList } from '@/util/timeSpan'
 
-const colorMap: any = {
-  网易云音乐: '#ff6262',
-  QQ音乐: '#ff9833',
-  酷我音乐: '#ffce59',
-  酷狗音乐: '#88aaf6',
-  other: '#4cc8d0',
-}
+// const colorMap: any = {
+//   网易云音乐: '#ff6262',
+//   QQ音乐: '#ff9833',
+//   酷我音乐: '#ffce59',
+//   酷狗音乐: '#88aaf6',
+//   other: '#4cc8d0',
+// }
 
 const colorList = [
   '#88aaf6',
   '#4cc8d0',
   '#c76dd9',
+  '#ff6262',
+  '#ff9833',
+  '#ffce59',
 ]
 const colorCount = colorList.length
 
@@ -27,6 +30,7 @@ export function releaseDayList(day: number) {
   return result
 }
 
+// 暂时不用 autoColor，默认全部为 autoColor
 export function dealDailyData(
   day: number,
   list: PlayData[],
@@ -54,7 +58,8 @@ export function dealDailyData(
           tooltipTitle: title,
         }
       }),
-      color: autoColor ? colorList[index % colorCount] : (colorMap[name] || colorMap.other)
+      color: colorList[index % colorCount],
+      // color: autoColor ? colorList[index % colorCount] : (colorMap[name] || colorMap.other)
     }
     return item
   })

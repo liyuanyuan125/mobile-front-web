e2ddf0798a2d736c2e57b42c3ad59739b44ca5ba
张鹏 <lip2up@qq.com>
2020-04-14 19:43:40 +0800

    fixbug

--- src/views/sentiment/music/components/playStats/playStats.vue
+++ src/views/sentiment/music/components/playStats/playStats.vue
@@ -94,6 +94,7 @@
         :data="table.data"
         :columns="table.columns"
         class="daily-table-wrap"
+        ref="table"
       />
       <a
         class="daily-form-more"
@@ -292,6 +293,11 @@ export default class PlayStats extends Vue {
     const view = await this.fetch(query)
     const list = Array.isArray(view) ? view : [ { label: '', view } ]
     this.viewList = list
+
+    this.$nextTick(() => {
+      const table = this.$refs.table as Table
+      table.updateUI()
+    })
   }
 
   handleFormMore() {

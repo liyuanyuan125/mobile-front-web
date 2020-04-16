b260219f2de78903ea90c9f278bfc1f0de961e0c
张鹏 <lip2up@qq.com>
2020-04-16 12:03:28 +0800

    fixbug && 优化基础设施

--- src/mock/api/music/song/1-detail.js
+++ src/mock/api/music/song/1-detail.js
@@ -24,10 +24,10 @@ mockGet('/yuqing/music/song/detail', ({ songId }) => {
     // 弹框简介
     basisDataList: [
       { name: '单曲名称1', value: 'Last Dance' },
-      { name: '单曲名称2', value: '挪威的森林2' },
+      { name: '单曲名称2', value: '' },
       { name: '单曲名称3', value: '挪威的森林3' },
-      { name: '单曲名称4', value: '挪威的森林4' },
-      { name: '单曲名称5', value: '挪威的森林5' },
+      { name: '单曲名称4', value: null },
+      { name: '单曲名称5', value: 0 },
       { name: '单曲名称6', value: '网易云音乐，QQ音乐，酷狗音乐，酷我音乐，虾米音乐' },
       { name: '单曲名称7', value: '挪威的森林7' },
       { name: '单曲名称8', value: '挪威的森林8' },
